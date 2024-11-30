0fecfcef5f1f0c885089cb619e8aae36df708133
Jean Pierre <>
2024-11-30 13:45:10 -0500

    initial swift package commit.

---
+++ Package.swift
@@ -0,0 +1,20 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "tellerkit",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "tellerkit",
+            targets: ["tellerkit"]),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "TellerKit",
+            path: "./TellerKit.xcframework" // Relative path to your xcframework
+        )
+    ]
+)
